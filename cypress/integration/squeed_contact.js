d8a58cea356ac42d510a6bfd8038726ee4212125
dedmau5 <ekendahl@gmail.com>
2019-03-12 18:24:32 +0100

    changed url: again...

--- cypress/integration/squeed_contact.js
+++ cypress/integration/squeed_contact.js
@@ -1,7 +1,7 @@
 describe('Find how to contact certain squeeders', function() {
     it('Goto Squeed url and look for a menu button ', function() {
         //cy.viewport(1280, 800)        
-        cy.visit('https://www.squeed.com');
+        cy.visit('https://squeed.com/');
 
         //Click on the 'Kontakta Oss'-menu item in the top menu and assert that the new url is correct
         cy.get(' li.nav-item:nth-child(4) > a.nav-link').click();
