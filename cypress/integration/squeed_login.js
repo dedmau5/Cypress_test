da79c9d6ba545900d4c9d90d581a78378490a0be
dedmau5 <ekendahl@gmail.com>
2019-03-12 13:29:30 +0100

    some restructuring

--- cypress/integration/squeed_login.js
+++ cypress/integration/squeed_login.js
@@ -2,7 +2,6 @@ describe('Find how to contact certain squeeders', function() {
     it('Goto Squeed url and look for a menu button ', function() {
         //cy.viewport(1280, 800)        
         cy.visit('https://www.squeed.com');
-
         cy.get('#navbarDropdown').click();
 
         //uncomment the corresponding optins
