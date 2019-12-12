0361ee4b742f737aa7c8f2fc680e446da43d64e7
Joar Ekendahl <joar.ekendahl@uc.se>
2019-12-12 16:45:38 +0100

    added minuc test and disabled chromeWebSecurity

---
+++ cypress/integration/minuc_login.js
@@ -0,0 +1,34 @@
+describe('Testing the login function on minuc.se', function() {
+    it('Goto minuc-at-url, click login button and log in', function() {
+        //cy.viewport(1280, 800)        
+        cy.visit('https://www1.minuc.se')
+            .get('.site-header__top .btn').click()
+            //.contains('#UP')
+            .url().should('include', 'weblogin/login?')
+            .get('#UP').click()
+            .get('#username.form-control').type('198409296376')
+            .get('#password.form-control').type('testMinuc2')
+            .get('form').submit()
+            .get('list-group-item')
+
+        /* cy.get('#navbarDropdown').click();
+
+        //uncomment the corresponding optins
+        cy.contains('Logga in').click();
+        //cy.contains('Glömt lösenord').click();
+        //cy.contains('Begär inloggning').click();
+        
+        cy.url().should('include', 'login/');
+        //cy.url().should('include', 'gloemt-loesenord/');
+        //cy.url().should('include', 'begaer-inloggning/');
+
+        //input credentials
+        cy.get('#Email').type('joar@squeed.com');
+        cy.get('#Password').type('Vinter56');
+        cy.get('form').submit();
+
+        //assert that user Joar E is logged in
+        cy.get('[id*="navbarDropdownPortfolio"]').should('contain', 'Joar E'); */
+        
+    })
+  })
