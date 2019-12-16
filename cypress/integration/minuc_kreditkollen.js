fa17f0d8044a713b38fab32171bbe6c9a81e1f8a
Joar Ekendahl <joar.ekendahl@uc.se>
2019-12-16 14:37:59 +0100

    added test for entering Kreditkollen

---
+++ cypress/integration/minuc_kreditkollen.js
@@ -0,0 +1,21 @@
+describe('Testing the login function on minuc.se', function() {
+    it('Goto minuc-at-url, click login button and log in', function() {
+        //cy.viewport(1280, 800)        
+        cy.visit('https://www1.minuc.se')
+            .get('.site-header__top .btn').click()
+            .url().should('include', 'weblogin/login?')
+            
+            .get('#UP').click()
+            .get('#username.form-control').type('198409296376')
+            .get('#password.form-control').type('testMinuc2')
+            .get('form').submit()
+
+            .url().should('include', 'weblogin/uploginhandler')
+            .get('div:nth-child(2) > div > div > div > a:nth-child(1)').click()
+            
+            .url().should('include', 'mina-tjanster')
+            .get('#credit-watch-product-head div div div a').click()
+            .get('div.introprogress').should('be.visible')
+        
+    })
+  })
