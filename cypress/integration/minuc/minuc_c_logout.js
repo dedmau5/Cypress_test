13cd5d5edf0f81f90ddeb1ab07779c1fdbd31135
Joar Ekendahl <joar.ekendahl@uc.se>
2020-01-07 16:29:25 +0100

    removed examples and updated package-lock.json after reinstall

--- cypress/integration/minuc/minuc_c_logout.js
+++
@@ -1,9 +0,0 @@
-describe('Testing the login function on minuc.se', function() {
-    it('Goto minuc-at-url, click logout button and verify log out', function() {
-        //cy.viewport(1280, 800)
-        cy.visit('https://www1.minuc.se')        
-        cy.get('#userdropdown').click()
-            .get('.signout').click()
-            .get('.site-header__top .btn').should('be.visible')
-    })
-  })
