2997040be9e412a190b99cdcd8475b8b74e1e4ae
Joar Ekendahl <joar.ekendahl@uc.se>
2020-01-07 16:28:04 +0100

    changed title for clarification and file_name for order

---
+++ cypress/integration/minuc/minuc_c_login.js
@@ -0,0 +1,20 @@
+describe('Testing the login function on minuc.se', function() {
+    it.skip('Goto minuc-at-url, click login button and log in', function() {
+        //cy.viewport(1280, 800)        
+        cy.visit('https://www1.minuc.se')
+            .get('.site-header__top .btn').click()
+            //.contains('#UP')
+            .url().should('include', 'weblogin/login?')
+            
+            .get('#UP').click()
+            .get('#username.form-control').type('198409296376')
+            .get('#password.form-control').type('testMinuc2')
+            .get('form').submit()
+            //.contains('Privattjänster').click()
+            .url().should('include', 'weblogin/uploginhandler')
+            
+            .get('div:nth-child(2) > div > div > div > a:nth-child(1)').click()
+            .url().should('include', 'mina-tjanster')
+            
+    })
+  })
