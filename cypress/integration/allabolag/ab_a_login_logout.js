f4ad524a1802872b1b247cc04e3ed762194ff20d
Joar Ekendahl <joar.ekendahl@uc.se>
2020-01-10 16:43:55 +0100

    adding steps to combat stupid iframe... :(

--- cypress/integration/allabolag/ab_a_login_logout.js
+++ cypress/integration/allabolag/ab_a_login_logout.js
@@ -1,3 +1,5 @@
+import '../../support/commands.js'
+
 describe('Testing happy-flow Allabolag, login, verify state and then logout', function() {
     it('Goto allabolag-at-url, click login button and log in, verify logged in state and then log out and verify logged out state', function() {
         //cy.viewport(1280, 800)        
@@ -17,19 +19,32 @@ describe('Testing happy-flow Allabolag, login, verify state and then logout', fu
             
             .url().should('include', '/konto?')
             
-            /*.get("iframe").then(($iframe) => {
-                const doc = $iframe.contents()
-                doc.find('div.container > div > button').click()
-            })
-            */
-            //.wrap(doc.find('div.container > div > button')).click({force:true})
-
-            //.get('div.container > div > button').click()
-            //.get('div > div > div .site-header__backlink > a > i').click()
-
+            .get('iframe')
+            .iframeLoaded()
+            //.itsDocument('document')
+            .getInDocument('div.container > div > button')
+            .trigger('click')
             
-            //.get('div.container > div.btn-group.site-header__login.show > ul > li.site-header__dropdownbutton > a').click()
-            //.get('.signout').click()
+
+    /*         .get("iframe").then(($iframe) => {
+                const $doc = $iframe.contents()
+                doc.find('div.container > div > button').click()
+                
+                //doc.find('div.container > div > ul > .site-header__dropdownbutton').click()
+                //cy.wrap(doc.find('div.container > div > button')).click({force:true})
+            }) */
+            
+            /* 
+            .get("iframe").then(($iframe) => {
+                const doc = $iframe.contents()
+                doc.find('div.container > div > ul > .site-header__dropdownbutton').click()
+                //cy.wrap(doc.find('div.container > div > button')).click({force:true})
+            })
+ */
+            //doc.find('div.container > div > button').click()
+                //.get('div > div > div .site-header__backlink > a > i').click()
+                
+                //.get('.signout').click()
 
             //.get('.site-header__top .btn').should('be.visible')
         
