c66ceee0de9c7bdb8cc3e87b006745585cccc480
Joar Ekendahl <joar.ekendahl@uc.se>
2020-01-14 15:08:26 +0100

    added test for searching a specific company and verifying the result

---
+++ cypress/integration/allabolag/ab_b_search.js
@@ -0,0 +1,14 @@
+import '../../support/commands.js'
+
+describe('Testing happy-flow Allabolag, search function', function() {
+    it('Goto allabolag-at-url, search for Volvo and verify search result', function() {
+        //cy.viewport(1280, 800)        
+        cy.visit('https://app-at-allabolag.platform.uc.se/')
+            .get('div.tw-flex.tw-items-center.tw-flex-1 > form > div > input').type('Volvo')
+            .get('form > div > button').click()
+            .url().should('include', 'what/Volvo')
+            .get('a').contains('Volvo Car AB').click()
+            .url().should('include', 'volvo-car-ab')
+
+    })
+  })
