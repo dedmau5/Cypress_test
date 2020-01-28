17530b7aa1a02784d1ff2f2f8a0132ecd6bcefdb
Joar Ekendahl <joar.ekendahl@uc.se>
2020-01-28 15:30:42 +0100

    changed selector to more robust ones

--- cypress/integration/allabolag/ab_b_search.js
+++ cypress/integration/allabolag/ab_b_search.js
@@ -3,8 +3,11 @@ import '../../support/commands.js'
 describe('Testing happy-flow Allabolag, search function', function() {
     it('Goto allabolag-at-url, search for Volvo and verify search result', function() {   
         cy.visit('https://app-at-allabolag.platform.uc.se/')
-            .get('div.tw-flex.tw-items-center.tw-flex-1 > form > div > input').type('Volvo')
-            .get('form > div > button').click()
+            //.get('div.tw-flex.tw-items-center.tw-flex-1 > form > div > input').type('Volvo')
+            //.get('form > div > button').click()
+            
+            .get('form.search-form input').type('Volvo')
+            .get('form.search-form button[type="submit"]').click()
             
             .url().should('include', 'what/Volvo')
             .get('a').contains('Volvo Car AB').click()
