f4f0dac23e2a9cf3cafdbcd809ee9af005834d7e
Joar Ekendahl <joar.ekendahl@uc.se>
2020-01-28 16:13:09 +0100

    removed some outcommented code

--- cypress/integration/minuc/minuc_a_login_logout.js
+++ cypress/integration/minuc/minuc_a_login_logout.js
@@ -2,9 +2,8 @@ describe('Testing happy-flow Minuc, login, verify state and then logout', functi
     it('Goto minuc-at-url, click login button and log in, verify logged in state and then log out and verify logged out state', function() {       
         cy.visit('https://www1.minuc.se')
             .get('.site-header__top .btn').click()
-            //.contains('#UP')
             .url().should('include', 'weblogin/login?')
-            
+
             .get('#UP').click()
             .get('#username.form-control').type('198409296376')
             .get('#password.form-control').type('testMinuc2')
