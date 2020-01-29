8a2cbd0b0a224ef635d557320f3cbf114cb36357
Joar Ekendahl <joar.ekendahl@uc.se>
2020-01-29 16:22:36 +0100

    removed some comments

--- cypress/integration/allabolag/ab_b_search.js
+++ cypress/integration/allabolag/ab_b_search.js
@@ -3,9 +3,6 @@ import '../../support/commands.js'
 describe('Testing happy-flow Allabolag, search function', function() {
     it('Goto allabolag-at-url, search for Volvo and verify search result', function() {   
         cy.visit('https://app-at-allabolag.platform.uc.se/')
-            //.get('div.tw-flex.tw-items-center.tw-flex-1 > form > div > input').type('Volvo')
-            //.get('form > div > button').click()
-            
             .get('form.search-form input').type('Volvo')
             .get('form.search-form button[type="submit"]').click()
             
