a307705316f7530a4627fcb2378cb4ed85ec25bc
Joar Ekendahl <joar.ekendahl@uc.se>
2020-02-13 16:10:35 +0100

    changed name to get rid of swedish chars

---
+++ cypress/integration/minuc/minuc_f_kop_i_utloggat_lage.js
@@ -0,0 +1,17 @@
+describe('Purchase when not logged in', function() {
+    it('Goto minuc-at-url, purchase a product ', function() {     
+        cy.visit('https://www1.minuc.se/mitt-kreditbetyg')
+        
+        //fill out order form
+        .get('div.order-form').should('be.visible')
+        .get('.form-control[name="personnummer"]').type('198409296376')
+        .get('.form-control[name="epost"]').type('testapan.gil@uc.se')
+        .get('.form-control[name="mobiltelefon"]').type('0701111111')
+        .get('.order-form .btn').click()
+        
+        //interact with giant pop-up
+        .get('body > div.order-layer > .form').should('be.visible')
+
+        
+    })
+  })
