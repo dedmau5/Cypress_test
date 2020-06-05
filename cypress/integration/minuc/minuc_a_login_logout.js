02fd9858bd61c28862b7c362a4d7540bd7663538
Joar Ekendahl <joar.ekendahl@uc.se>
2020-06-05 14:12:06 +0200

    added a fix for uc-test that failed during login in SAAM, and restructured minuc-login a bit

--- cypress/integration/minuc/minuc_a_login_logout.js
+++ cypress/integration/minuc/minuc_a_login_logout.js
@@ -1,21 +1,29 @@
-describe('Testing happy-flow Minuc, login, verify state and then logout', function() {
-    it('Goto minuc-at-url, click login button and log in, verify logged in state and then log out and verify logged out state', function() {       
+describe('Testing happy-flow Minuc, login, verify state and then logout', () => {
+    
+    it('visit minuc AT url, verify state', () => {    
         cy.visit('https://www1.minuc.se')
-            .get('.site-header__top .btn').click()
-            .url().should('include', 'weblogin/login?')
+            .url().should('include', 'www1.minuc.se')
+    })
 
-            .get('#UP').click()
+    it('start login flow and verify state', () => {
+        cy.get('.site-header__top .btn').click()
+            .url().should('include', 'weblogin/login?')
+    })
+
+    it('Inside SAAM login, fill out forms and proceed through the flow', () => {        
+        cy.get('#UP').click()
             .get('#username.form-control').type('198409296376')
             .get('#password.form-control').type('testMinuc2')
             .get('form').submit()
             //.contains('Privattjänster').click()
             .url().should('include', 'weblogin/uploginhandler')
             .get('div:nth-child(2) > div > div > div > a:nth-child(2)').click()
-            
-            .url().should('include', 'mina-tjanster')
+    })
+    
+    it('back on minuc, verify state, logout, verify state', () => {
+        cy.url().should('include', 'mina-tjanster')
             .get('#userdropdown').click()
             .get('.signout').click()
             .get('.site-header__top .btn').should('be.visible')
-        
-    })
+        })
   })
--- cypress/integration/uc/uc_a_login_logout.js
+++ cypress/integration/uc/uc_a_login_logout.js
@@ -12,11 +12,13 @@ describe('Testing happy-flow UC, login, verify state and logout', () => {
         cy.get('#username').type('195008196015')
         cy.get('#password').type('testMinuc4')
         cy.get('#loggainperson').click()
+        cy.url().should('include', 'weblogin/uploginhandler')
+        cy.get('div:nth-child(2) > div > div > div > a:nth-child(2)').click()
     })
  
     it('confirm user login', () => {
-        cy.url().should('include', 'weblogin/uploginhandler')
-        cy.get('div:nth-child(2) > div > div > div > a:nth-child(2)').click()
+        //cy.url().should('include', 'weblogin/uploginhandler')
+        //cy.get('div:nth-child(2) > div > div > div > a:nth-child(2)').click()
         cy.url().should('include', 'mina-tjanster/uc')
     })
  
